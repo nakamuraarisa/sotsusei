4fee3d460633ce8c4e7042680a85b28716069d74
nakamuraarisa <nakamura.arisa57@gmail.com>
2024-12-27 13:06:24 +0900

    検索フォーム作成（見た目のみ）、ナビゲーションバーのhover時カラー変更（アイコンと文字が別々問題は残っている）

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,31 +62,41 @@
         <div class="footer-nav">
             <a href="#">
                 <div class="icon-container">
-                    <img src="{{ asset('images/home.jpeg') }}" alt="ホーム" width="24" height="24">
+                    <img src="{{ asset('images/home.jpeg') }}" alt="ホーム" width="24" height="24" class="icon_home"
+                    onmouseover="this.src='{{ asset('images/home_yellow.jpeg') }}';"
+                    onmouseout="this.src='{{ asset('images/home.jpeg') }}';">
                 </div>
                 ホーム
             </a>
             <a href="#">
                 <div class="icon-container">
-                    <img src="{{ asset('images/heart1.jpeg') }}" alt="お気に入り" width="24" height="24">
+                    <img src="{{ asset('images/heart1.jpeg') }}" alt="お気に入り" width="24" height="24" class="icon_heart"
+                    onmouseover="this.src='{{ asset('images/heart1_yellow.jpeg') }}';"
+                    onmouseout="this.src='{{ asset('images/heart1.jpeg') }}';">
                 </div>
                 お気に入り
             </a>
             <a href="#">
                 <div class="icon-container">
-                    <img src="{{ asset('images/message.jpeg') }}" alt="メッセージ" width="24" height="24">
+                    <img src="{{ asset('images/message.jpeg') }}" alt="メッセージ" width="24" height="24"  class="icon_message"
+                    onmouseover="this.src='{{ asset('images/message_yellow.jpeg') }}';"
+                    onmouseout="this.src='{{ asset('images/message.jpeg') }}';">
                 </div>
                 メッセージ
             </a>
             <a href="#">
                 <div class="icon-container">
-                    <img src="{{ asset('images/bell.jpeg') }}" alt="お知らせ" width="24" height="24">
+                    <img src="{{ asset('images/bell.jpeg') }}" alt="お知らせ" width="24" height="24" class="icon_bell"
+                    onmouseover="this.src='{{ asset('images/bell_yellow.jpeg') }}';"
+                    onmouseout="this.src='{{ asset('images/bell.jpeg') }}';">
                 </div>
                 お知らせ
             </a>
             <a href="#">
                 <div class="icon-container">
-                    <img src="{{ asset('images/setting.jpeg') }}" alt="設定" width="24" height="24">
+                    <img src="{{ asset('images/setting.jpeg') }}" alt="設定" width="24" height="24" class="icon_setting"
+                    onmouseover="this.src='{{ asset('images/setting_yellow.jpeg') }}';"
+                    onmouseout="this.src='{{ asset('images/setting.jpeg') }}';">
                 </div>
                 設定
             </a>
--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,5 +1,32 @@
 <x-app-layout>
-    <div>
-        <p>ホーム画面のコンテンツをここに記述</p>
+
+    <!-- パンくずリスト -->
+    {{-- <div class="breadcrumb">
+        <span>ホーム > 検索</span>
+    </div> --}}
+
+    <!-- 検索フォーム -->
+    <div class="search-section">
+        <form action="/search" method="GET" class="search-form">
+
+            <!-- 日付検索 -->
+            <div class="form-group">
+                <label for="date" class="form-label">
+                    日付
+                </label>
+                <input type="date" id="date" name="date" class="form-input">
+            </div>
+
+            <!-- 日付未定チェック -->
+            <div class="form-group">
+                <input type="checkbox" id="no-date" name="no_date" class="form-checkbox">
+                <label for="no-date" class="form-label">日付未定</label>
+            </div>
+
+            <!-- 検索ボタン -->
+            <button type="submit" class="btn-search">検索する</button>
+
+        </form>
+    
     </div>
 </x-app-layout>
