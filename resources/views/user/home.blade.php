427333102fca5196da0c26ffacd6304fa7e234b7
nakamuraarisa <nakamura.arisa57@gmail.com>
2025-01-06 15:19:44 +0900

    検索結果をカード型に表示

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -32,17 +32,21 @@
             @if($events->isEmpty())
                 <p>指定された日程に開催されるイベントはありません。</p>
             @else
-                <ul>
+                <div class="card-container">
                     @foreach ($events as $event)
-                        <li>
-                            <img src="{{ asset($event->image_path) }}" alt="写真" width=300>
-                            <h3>{{ $event->title }}</h3>
-                            <p>場所： {{ $event->place }}</p>
-                            <p>時間： {{ $event->start_time }}〜{{ $event->end_time }}</p>
-                            <p>報酬： {{ $event->reward }}円/日</p>
-                        </li>
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="{{ asset($event->image_path) }}" alt="写真" width=300>
+                            </div>
+                            <div class="card-content">
+                                <h3 class="card-title">{{ $event->title }}</h3>
+                                <p class="card-description">場所：{{ $event->place }}</p>
+                                <p class="card-description">時間：{{ $event->start_time }}〜{{ $event->end_time }}</p>
+                                <p class="card-description">報酬：{{ $event->reward }}円/日</p>
+                            </div>
+                        </div>
                     @endforeach
-                </ul>
+                </div>
             @endif
         </div>
     @endif
