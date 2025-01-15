f9fba28c8db92d7fad3406970979256527afc666
nakamuraarisa <nakamura.arisa57@gmail.com>
2025-01-15 15:45:14 +0900

    設定ページにログアウトボタンを追加し、CSSを修正

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,9 +1,9 @@
 <x-app-layout>
-    <x-slot name="header">
+    {{-- <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Profile') }}
         </h2>
-    </x-slot>
+    </x-slot> --}}
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -20,6 +20,18 @@
                     </div>
                 </div>
 
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg w-full max-w-3xl">
+                    <div class="flex flex-col items-center space-y-4"> <!-- flex-colで縦並びに変更、余白を追加 -->
+                        <h2 class="text-lg font-medium text-gray-900">
+                            ログアウト
+                        </h2>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <x-secondary-button>{{ __('ログアウト') }}</x-secondary-button>
+                        </form>
+                    </div>                    
+                </div>
+                
                 <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg w-full max-w-3xl"> <!-- 幅を調整 -->
                     <div class="max-w-full">
                         @include('profile.partials.delete-user-form')
